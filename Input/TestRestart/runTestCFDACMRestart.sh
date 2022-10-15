91c581d57be5ba7a5f04f4d1d38625065935e4ca
Zheng Gong <gongzheng_justin@outlook.com>
2022-10-15 09:33:03 +0800

    Add files via upload

---
+++ Input/TestRestart/runTestCFDACMRestart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+nproc=8
+appName="mpirun"
+
+cd ../../../
+chmod a+x mymake.sh
+./mymake.sh -exe-channelACM -cmp-gcc_MPI -CompileThirdParty-0 -deleteCompileFile-1 -CFD_DEFS_Add-DFFTW_1D -ACM_DEFS_Add -CFDACM_DEFS_Add
+
+mkdir -p ./ACM/Restart/ 2> /dev/null
+cp ./Input/CFDACM/TestRestart/SpheresCoord.Ji ./ACM/Restart/SpheresCoord.dat
+
+$appName -n $nproc ./channelACM ./Input/CFDACM/TestRestart/SphereJi.cfd01 ./Input/CFDACM/TestRestart/SphereJi.acm01
+rm -rf CFD/Restart/RestartForJiSedPOF020000000000
+rm -rf ACM/Restart/RestartForJiSedPOF_P0000000000
+mv CFD/Restart/RestartForJiSedPOF010000000010 CFD/Restart/RestartForJiSedPOF020000000000
+mv ACM/Restart/RestartForJiSedPOF_P0000000010 ACM/Restart/RestartForJiSedPOF_P0000000000
+$appName -n $nproc ./channelACM ./Input/CFDACM/TestRestart/SphereJi.cfd02 ./Input/CFDACM/TestRestart/SphereJi.acm02
+cp ./CFD/Results/JiSedPOF02.log JiSedPOF02_backup.txt
