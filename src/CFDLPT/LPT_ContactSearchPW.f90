f0d10f759b3919c177132ff4d9365c98dfc949f6
Zheng Gong <gongzheng_justin@outlook.com>
2022-12-26 09:38:25 +0800

    Add files via upload

---
+++ src/CFDLPT/LPT_ContactSearchPW.f90
@@ -0,0 +1,55 @@
+module LPT_ContactSearchPW
+  use MPI
+  use m_TypeDef
+  use LPT_Property
+  use LPT_Geometry
+  use LPT_Variables
+  use LPT_Parameters
+  use m_Decomp2d,only: nrank
+  use m_Parameters,only:yly,xlx,zlz  
+  use LPT_Decomp_2d,only:int_type,real_type
+  implicit none
+  private
+
+  type::ContactSearchPW
+  contains
+    procedure:: FindContactsPW
+  end type ContactSearchPW
+  type(ContactSearchPW),public::LPTContactSearchPW
+    
+contains
+
+  !**********************************************************************
+  ! Performing contact search to determine particle-wall contacts 
+  !**********************************************************************
+  subroutine FindContactsPW(this)
+    implicit none
+    class(ContactSearchPW):: this
+
+    ! locals
+    integer:: pid
+    real(RK)::Posy,radius
+
+#ifdef UseDEMWallContact
+    integer::wid
+    DO pid=1,GPrtcl_list%nlocal
+      do wid=1,LPTGeometry%nPW_local
+         if(LPTGeometry%pWall(wid)%isInContact(GPrtcl_PosR(pid),ovrlp,nv))then
+           GPrtcl_PosR(pid)%y=  GPrtcl_PosOld(pid)%y
+           GPrtcl_linVel(1,pid)%y= - GPrtcl_linVel(2,pid)%y               
+         endif 
+      enddo
+    ENDDO
+#else
+    DO pid=1,GPrtcl_list%nlocal
+      Posy=GPrtcl_PosR(pid)%y
+      radius= GPrtcl_PosR(pid)%w
+      if(Posy<=radius .or. Posy+radius>=yly) then
+        GPrtcl_PosR(pid)%y    =   GPrtcl_PosOld(pid)%y
+        GPrtcl_linVel(1,pid)%y= - GPrtcl_linVel(2,pid)%y
+      endif
+    ENDDO
+#endif
+  end subroutine FindContactsPW
+    
+end module LPT_ContactSearchPW
