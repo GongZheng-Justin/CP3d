a114eb830abba4ab7f4efc1520f1496a646113d8
Zheng Gong <gongzheng_justin@outlook.com>
2026-05-25 16:49:16 +0800

    Add files via upload

---
+++ input/CFD_ACM/ParticleFalling/runParticleFalling.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cmpStr="gcc"
+appStr="mpirun"
+p_row=4
+p_col=2
+if [[ -n $1 ]]; then
+  cmpStr=$1
+fi
+if [[ -n $2 ]]; then
+  appStr=$2
+fi
+if [[ -n $3 ]]; then
+  p_row=$3
+fi
+if [[ -n $4 ]]; then
+  p_col=$4
+fi
+let nproc=p_row*p_col
+
+cd ../../../
+chmod a+x mymake.sh
+mkdir -p ./ACM/Restart/ 2> /dev/null
+
+./mymake.sh -exe-cfd_ACM -cmp-"$cmpStr"_MPI -CompileThirdParty-0 -deleteCompileFile-1 -CFD_DEFS_Add -ACM_DEFS_Add -CFDACM_DEFS_Add-DSeveralSphereInfo
+
+cp -rf ./input/CFD_ACM/ParticleFalling/SpheresCoord.Cate ./ACM/Restart/SpheresCoord.txt
+for loop in 4 3 2 1
+do
+  $appStr -n $nproc ./cfd_ACM ./input/CFD_ACM/ParticleFalling/SphereCate.cfd"$loop" ./input/CFD_ACM/ParticleFalling/SphereCate.acm $p_row $p_col
+done
